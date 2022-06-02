cf7945a992b31f2d6e63ed7cf74a1bafed6a8e1d
mariannedutra <mariannedutra@gmail.com>
2022-06-02 20:57:06 -0300

    another commit

--- rede_social/views.py
+++ rede_social/views.py
@@ -1,3 +1,18 @@
+
+from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Usuario
+
+def index(request):
+  return HttpResponse('Página Inicial')
+
+def perfil_template(request,slug):
+  try:
+    perfil = Usuario.objects.get(slug=slug)
+
+  except Usuario.DoesNotExist:
+    raise Http404('Perfil não encontrado')
+    
+  return render(request, 'atividade_inicial/perfil.html', {'perfil': perfil})
