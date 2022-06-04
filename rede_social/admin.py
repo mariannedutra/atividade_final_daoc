b948d25a5ac43244395d5fcc251d7d5582a1cc34
YanSardinha <yansardinha1001@gmail.com>
2022-06-04 11:37:40 -0300

    Corrigindo arquivos e adicionando função que conta seguidores

--- rede_social/admin.py
+++ rede_social/admin.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
+from rede_social.models import Pessoa, Postagem, Comentario
 
-from rede_social.models import Comentario, Postagens, RelacionamentoUsuarios, Usuario
 
-# Register your models here.
-
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
     exclude = ('slug',)
     pass
 
-@admin.register(Postagens)
-class PostagensAdmin(admin.ModelAdmin):
+@admin.register(Postagem)
+class PostagemAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Comentario)
-class Comentario(admin.ModelAdmin):
+class ComentarioAdmin(admin.ModelAdmin):
     pass
--- rede_social/models.py
+++ rede_social/models.py
@@ -2,18 +2,14 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
-
 class Pessoa(models.Model):
     nome = models.CharField('Nome', max_length=50)
-
     slug = models.SlugField('Slug', max_length=250)
-
     usuario = models.OneToOneField(
         User, 
         on_delete=models.CASCADE, 
         verbose_name='Usuário')
-    
-    amigos = models.ManyToManyField('Pessoa')
+    amigos = models.ManyToManyField('Pessoa', symmetrical=False, blank= True)
 
     def __str__(self):
         return self.nome
@@ -22,24 +18,40 @@ class Pessoa(models.Model):
         self.slug = slugify(self.nome)
         super(Pessoa, self).save(*args, **kwargs)
 
+    @property
+    def qt_seguidores(self):
+        "Retorna quantidade de pessoas que são seguidores"
+        return Pessoa.objects.filter(amigos=self).count()
+
+    
+    @property
+    def qt_seguindo(self):
+        "Retorna quantidade de pessoas que a pessoa está seguindo"
+        return Pessoa.objects.filter(amigos=self).count()
+    '''
+    Opções para conferir quantas pessoas o usuário está seguindo.
+    1 - Filtrar para pegar o count de quantas vezes o usuário aparece como seguidor para outra pessoa (Pesado em larga escala).
+    2 - Criar variável que captura quem você está seguindo, e fazer um count.
+    '''
 
 class Postagem(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, verbose_name="Pessoa" )
+    conteudo = models.CharField('Conteudo', max_length=200)
+    data = models.DateTimeField("Data de publicação", auto_now_add=True)
 
-  pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, verbose_name="Usuario" )
+    def __str__(self):
+        return self.conteudo
 
-  conteudo = models.CharField('Conteudo', max_length=200)
-
-  data = models.DateTimeField("Data de publicação", auto_now_add=True)
-
-  def __str__(self):
-    return self.conteudo
+    @property
+    def qt_comentarios(self):
+        "Retorna quantidade de comentarios"
+        return Comentario.objects.filter(postagem=self).count()
 
 class Comentario(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, verbose_name="Pessoa")
+    postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE, verbose_name="Postagem")
+    conteudo = models.CharField('Comentario', max_length=200)
+    data = models.DateTimeField("Data de publicação", auto_now_add=True)
 
-  pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE, verbose_name="Usuario")
-  postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE, verbose_name="Postagens")
-  conteudo = models.CharField('Comentario', max_length=200)
-  data = models.DateTimeField("Data de publicação", auto_now_add=True)
-  
-  def __str__(self):
-     return self.conteudo
+    def __str__(self):
+        return self.conteudo
